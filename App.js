47d3f07f458b51684f6ffba9de80ebf01740dd55
Karen-Mich <kguatumillo3914@uta.edu.ec>
2023-12-10 12:35:55 -0500

    Llamado de RestaurantTables en la pagina principal

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './screens/formulario_Log';    
+import Login from './screens/formulario_Log';
+/*import RestaurantTables from './screens/mesas';    */
 
 
 export default function App() {
   return (
       <Login/>
+    /*  <RestaurantTables/>*/
   );
 }
 
